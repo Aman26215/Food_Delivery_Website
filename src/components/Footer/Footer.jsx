e573f2bf96aa9957bcf5434a14b45e35aab57b1a
Aman Ranjan <ranjanaman4@gmail.com>
2024-09-11 23:53:27 +0530

    Minor bug fixed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
       <div className="footer-content">
         <div className="footer-content-left">
         <img src={assets.logo} alt="" />
-        <p>afasfsafkf</p>
+        <p>You Order, We Deliver</p>
         <div className="footer-social-icon">
           <img src={assets.facebook_icon} alt="" />
           <img src={assets.twitter_icon} alt="" />
